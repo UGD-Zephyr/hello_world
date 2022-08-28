228afa916e69ca1e5759800b9602fae5f0d2449b
Zephyr <ugd.zephyr@gmail.com>
2022-08-28 09:36:44 +0200

    added hello world c program.

---
+++ hello_world.c
@@ -0,0 +1,14 @@
+/* Programmer: Per Stoor
+ * Date: 2022-08-28
+ * Last changed: 2022-08-28
+ * Type of program: testing git with hello_world.c
+ * */
+
+#include <stdio.h>
+
+int main(){
+
+	printf("Hello World!\n");
+
+return 0;
+}
